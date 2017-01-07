16404799ca19275f1f540c3e4decbd1e4f0459cd
Pedro Santos <donbonifacio@gmail.com>
2017-01-07 18:27:34 +0000

    Generate project via maven

---
+++ src/main/java/code/donbonifacio/App.java
@@ -0,0 +1,13 @@
+package code.donbonifacio;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
