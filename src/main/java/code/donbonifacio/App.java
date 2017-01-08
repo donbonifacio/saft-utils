052fcac8e5b811e28bc60f32ce3d0f882f099368
Pedro Santos <donbonifacio@gmail.com>
2017-01-08 18:29:33 +0000

    Added guava and slf4j as dependencies

--- src/main/java/code/donbonifacio/App.java
+++ src/main/java/code/donbonifacio/App.java
@@ -1,13 +1,17 @@
 package code.donbonifacio;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    static final Logger logger = LoggerFactory.getLogger(App.class);
+
+    public static void main( String[] args ) {
+        logger.info("SAF-T Utilities");
     }
+
 }
