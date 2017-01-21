f68919331a8d88beabf3debc36bf8058afb3e17f
Pedro Santos <donbonifacio@gmail.com>
2017-01-21 14:50:08 +0000

    Add more tests to Result

--- src/test/java/code/donbonifacio/saft/ResultTest.java
+++ src/test/java/code/donbonifacio/saft/ResultTest.java
@@ -2,6 +2,8 @@ package code.donbonifacio.saft;
 
 import junit.framework.TestCase;
 
+import java.util.Arrays;
+
 /**
  * Tests for the Result class
  */
@@ -30,5 +32,28 @@ public class ResultTest extends TestCase {
         Result result = Result.exception(ex);
         assertFalse(result.isSucceeded());
         assertTrue(result.isException());
+    }
+
+    /**
+     * Test toString generation
+     */
+    public void testToString() {
+        Result r1 = Result.failure("Test1");
+        Result r2 = Result.failure("Test2");
+        Result r3 = Result.fromResults(Arrays.asList(r1, r2));
+
+        assertEquals("Result{succeeded=false, reason='From list of results', results=[Result{succeeded=false, reason='Test1'}, Result{succeeded=false, reason='Test2'}, ]}", r3.toString());
+    }
+
+    /**
+     * If fromResults gets a collection with only one result, return
+     * that result
+     */
+    public void testFromResultsAsOne() {
+        Result r1 = Result.success();
+        Result r2 = Result.fromResults(r1.asList());
+
+        assertTrue(r1 == r2);
+
     }
 }
